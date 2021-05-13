9ae0eb834aa39ec162e64d68d190615c9ba5c816
ggulbani <ggulbani@ggulbanis-MacBook-Pro.local>
2021-05-13 04:28:39 +0400

    added edit and update functionality

--- app/controllers/p_folios_controller.rb
+++ app/controllers/p_folios_controller.rb
@@ -3,6 +3,37 @@ class PFoliosController < ApplicationController
     @portfolio_items = PFolio.all
   end
 
+  def new
+    @portfolio_item = PFolio.new
+  end
 
+  def create
+    @portfolio_item = PFolio.new(params.require(:p_folio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to p_folios_path, notice: "Your Portfolio item now created." }
+        # format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit 
+    @portfolio_item = PFolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = PFolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:p_folio).permit(:title, :subtitle, :body))
+        format.html { redirect_to p_folios_path, notice: "Portfolio record was successfully updated." }
+      else
+        format.html { render :edit}
+      end
+    end
+  end
 
 end
